25e49d852f0324907a3a3379a08fa6dab221e86d
Dave JC <davejc99@gmail.com>
2015-02-19 19:54:59 +0530

    changes related to file upload

--- db/migrate/20150218091528_create_myuploaders.rb
+++
@@ -1,9 +0,0 @@
-class CreateMyuploaders < ActiveRecord::Migration
-  def change
-    create_table :myuploaders do |t|
-      t.attachment :image
-
-      t.timestamps null: false
-    end
-  end
-end
